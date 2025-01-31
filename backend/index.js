088ba3ce628d14552fd78efd4a649a17bdd4cca9
shivamsingh48 <shivamsinghr098@gmail.com>
2025-01-31 22:14:44 +0530

    server setup

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+import { connectMongoDB } from './db.js';
+import { app } from './app.js';
+
+dotenv.config();
+
+const PORT=process.env.PORT || 8000
+
+connectMongoDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server started at port - ",PORT);
+    })
+})
+.catch((error)=>{
+    app.on("error",(error)=>{
+        console.log("ERR: ",error);
+    })
+})
